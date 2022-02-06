664cacd14416ac59fa54fc01e1c0ab1d48ab1a35
SarahNurhasya <sarahnurhasya02@gmail.com>
2022-02-07 01:59:56 +0800

    HOSPITAL TRACKER APPS

--- app/src/main/java/com/hospitalsystem/MainActivity2.java
+++ app/src/main/java/com/hospitalsystem/MainActivity2.java
@@ -1,30 +1,52 @@
 package com.hospitalsystem;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 public class MainActivity2 extends AppCompatActivity {
 
-    Button button2;
+    CardView about_card,profile_card,find_card;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        button2 = (Button) findViewById(R.id.button2);
-
-        button2.setOnClickListener(new View.OnClickListener() {
+        find_card = (CardView) findViewById(R.id.find_card);
+        find_card.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent =new Intent() ;
+                Intent intent = new Intent();
                 intent.setClass(getApplicationContext(), MapsActivity.class);
                 startActivity(intent);
             }
         });
+
+        about_card = (CardView) findViewById(R.id.about_card);
+        about_card.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(getApplicationContext(), AboutActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        profile_card = (CardView) findViewById(R.id.profile_card);
+        profile_card.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(getApplicationContext(), ProfileActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
